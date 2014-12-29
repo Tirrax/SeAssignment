c78d6ae74a10455564bf9865cdb7acfc092472ee
Tirrax <Tirrax@2aaa1e41-5f46-304c-a516-ff6f95677b63>
2014-12-29 14:47:01 +0000

    Added paypal support
    git-svn-id: svn://localhost/trunk@7 2aaa1e41-5f46-304c-a516-ff6f95677b63

---
+++ SEWebiste_OwenAttard/SEWebiste_OwenAttard/Controllers/PayPalController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity.Migrations.Design;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BL;
+using Common;
+using Microsoft.Ajax.Utilities;
+using SEWebiste_OwenAttard.Models;
+
+namespace SEWebiste_OwenAttard.Controllers
+{
+    public class PayPalController : Controller
+    {
+        public ActionResult RedirectFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult NotifyFromPaypal()
+        {
+            return View();
+        }
+        public ActionResult ValidateCommand(string product, string totalPrice)
+        {
+            bool useSandbox = Convert.ToBoolean(ConfigurationManager.AppSettings["IsSandbox"]);
+            var paypal = new PayPalModel(useSandbox);
+
+            List<ShoppingCart> cart = new ShoppingCartBL().GetShoppingCartItems(User.Identity.Name).ToList();
+
+            foreach (var curCart in cart)
+            {
+                ItemModel model = new ItemModel()
+                {
+                    quantity = curCart.Qty,
+                    item_name = curCart.Product.Name,
+                    amount = curCart.Product.Price
+                };
+
+                paypal.items.Add(model);
+            }
+
+
+            return View(paypal);
+        }
+
+    }
+}
---
+++ SEWebiste_OwenAttard/SEWebiste_OwenAttard/Models/PayPalModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SEWebiste_OwenAttard.Models
+{
+    public class ItemModel
+    {
+        public string item_name { get; set; }
+        public decimal amount { get; set; }
+        public int quantity { get; set; }
+    }
+
+    public class PayPalModel
+    {
+        public string cmd { get; set; }
+        public string business { get; set; }
+        public string no_shipping { get; set; }
+        public string @return { get; set; }
+        public string cancel_return { get; set; }
+        public string notify_url { get; set; }
+        public string currency_code { get; set; }
+        public List<ItemModel> items = new List<ItemModel>(); 
+
+        public string actionURL { get; set; }
+
+        public PayPalModel(bool useSandBox)
+        {
+            cmd = "_cart";
+            business = ConfigurationManager.AppSettings["business"];
+            cancel_return = string.Format("http://{0}/Products/ShoppingCart", HttpContext.Current.Request.Url.Authority);
+            @return = string.Format("http://{0}/Products/ShoppingCart", HttpContext.Current.Request.Url.Authority);
+
+            if (useSandBox)
+            {
+                actionURL = ConfigurationManager.AppSettings["test_url"];
+            }
+            else
+            {
+                actionURL = ConfigurationManager.AppSettings["prod_url"];
+            }
+
+            notify_url = ConfigurationManager.AppSettings["notify_url"];
+            currency_code = ConfigurationManager.AppSettings["currency_code"];
+        }
+    }
+}
